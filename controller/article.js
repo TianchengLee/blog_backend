e768de70f73e083fa4cf1df1c0bc374159eb3022
TianchengLee <ltc6634284@gmail.com>
2018-11-07 14:33:07 +0800

    完成获取文章API,完善模糊查询的逻辑,支持文章标题文章内容用户名和昵称的模糊查询

--- controller/article.js
+++ controller/article.js
@@ -2,30 +2,25 @@ const moment = require('moment')
 const conn = require('../db/db.js')
 module.exports = {
   handleGetArticles(req, res) {
-    if (!req.query.hasOwnProperty('page') || !req.query.hasOwnProperty('pageSize')) return res.status(400).send({ status: 400, msg: '参数错误, 必须传入page和pageSize' })
-    let page = req.query.page
-    let pageSize = req.query.pageSize
-    let title = req.query.title || ''
-    let content = req.query.content || ''
-    let username = req.query.username || ''
-    let nickname = req.query.nickname || ''
+    // if (!req.query.hasOwnProperty('page') || !req.query.hasOwnProperty('pageSize')) return res.status(400).send({ status: 400, msg: '参数错误, 必须传入page和pageSize' })
+    let page = req.query.page || 1
+    let pageSize = req.query.pageSize || 10
+    let keywords = req.query.keywords || ''
 
-    console.log(req.query)
-
-    const queryArticlesSql = `select a.id as aid, a.title, a.content, a.ctime, u.id as uid, u.username, u.nickname from articles as a
+    const statementSql = `from articles as a
     left join users as u
     on a.author_id = u.id
-    where a.title like '%${title}%'
-    or a.content like '%${content}%'
-    or u.username like '%${username}%'
-    or u.nickname like '%${nickname}%'
-    order by aid desc
-    limit ${page}, ${pageSize}`
+    where concat(title, content, username, nickname) like '%${keywords}%'`
 
-    console.log(queryArticlesSql)
-    conn.query(queryArticlesSql, (err, result) => {
-      if (err) return res.status(500).send({status: 500, msg: '数据获取失败, 请重试!', data: null})
-      res.send({ status: 200, msg: 'ok', data: result });
+    const queryArticlesSql = `select a.id as aid, a.title, a.content, a.ctime, u.id as uid, u.username, u.nickname ${statementSql}
+    order by aid desc
+    limit ${(page - 1) * pageSize}, ${pageSize};`
+
+    const queryArticleTotalCountSql = `select count(*) as totalCount ${statementSql}`
+
+    conn.query(queryArticlesSql + queryArticleTotalCountSql, (err, result) => {
+      if (err) return res.status(500).send({ status: 500, msg: '数据获取失败, 请重试!', data: null })
+      res.send({ status: 200, msg: 'ok', totalCount: result[1][0].totalCount, data: result[0] });
     })
   },
   handleArticleAddPost(req, res) {
